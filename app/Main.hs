5b772d5f55ecf902e092c4b92b44faeaba16051b
Bibek Pandey <bibekp@gmail.com>
2025-07-03 14:47:25 -0700

    Added cook supervisor to restart failed cooks

--- app/Main.hs
+++ app/Main.hs
@@ -31,7 +31,9 @@ data OrderStatus = OrderStatus
     orderState :: OrderState
   }
 
-type OSMap = TVar (HM.Map Int OrderStatus)
+type StatusMap = TVar (HM.Map Int OrderStatus)
+
+newtype CookFail = CookFail Int
 
 nextState :: OrderStatus -> OrderStatus
 nextState os@(OrderStatus _ _ _ Raw) =
@@ -42,25 +44,25 @@ nextState os@(OrderStatus _ _ True Cooked) =
   os {orderState = Delivered}
 nextState os = os -- No change if conditions not met
 
-generateOrder :: Int -> TQueue Order -> IO String 
-generateOrder key queue = do
-  createdAt <- getCurrentTime
+generateOrder :: Int -> TQueue Order -> IO String
+generateOrder oid queue = do
+  now <- getCurrentTime
   condiments <- randomRIO (True, False)
   guac <- randomRIO (True, False)
   bType <- randomBurger
 
-  let order = Order createdAt key bType condiments guac
+  let order = Order now oid bType condiments guac
   atomically $ writeTQueue queue order
 
-  return $ "Placed new order " ++ show key ++ " (burgerType " ++ show bType ++ ")"
+  return $ "Placed new order " ++ show oid ++ " (burgerType " ++ show bType ++ ")"
 
 patron :: Int -> TQueue Order -> IO ()
-patron key queue = do
-  orderOutput <- generateOrder key queue
-  putStrLn $ "Patron " ++ show key ++ ", " ++ orderOutput
+patron pid queue = do
+  orderOutput <- generateOrder pid queue
+  putStrLn $ "Patron " ++ show pid ++ ", " ++ orderOutput
 
 -- Receives order from customerQ, places onto kitchenQ
-orderTaker :: TQueue Order -> TQueue Order -> OSMap -> IO ()
+orderTaker :: TQueue Order -> TQueue Order -> StatusMap -> IO ()
 orderTaker fromQ toQ statusMap = forever $ do
   order <- atomically $ readTQueue fromQ
   putStrLn $ " Received Order " ++ show (orderId order)
@@ -71,26 +73,41 @@ orderTaker fromQ toQ statusMap = forever $ do
   atomically $ writeTQueue toQ order
   return ()
 
-cook :: Int -> TQueue Order -> TQueue Order -> OSMap -> IO ()
-cook key fromQ toQ statusMap = cookLoop 2
+spawnCooks :: (Foldable t) => t Int -> TQueue Order -> TQueue Order -> StatusMap -> IO ()
+spawnCooks list fromQ toQ statusMap = do
+  cookFail <- newTChanIO
+  -- start cooks
+  forM_ list $ \cid -> forkIO $ cook cid fromQ toQ statusMap cookFail
+  -- launch cookSupervisor as a new thread to handle on demand any cook failures
+  void $ forkIO $ cookSupervisor fromQ toQ statusMap cookFail
+
+cookSupervisor :: TQueue Order -> TQueue Order -> StatusMap -> TChan CookFail -> IO ()
+cookSupervisor fromQ toQ statusMap cookFail = forever $ do
+  CookFail cid <- atomically $ readTChan cookFail
+  threadDelay =<< randomRIO (100_000, 200_000)
+  void $ forkIO $ cook cid fromQ toQ statusMap cookFail
+  putStrLn $ " Cook " ++ show cid ++ ", restarted and given the chance to cook again"
+
+cook :: Int -> TQueue Order -> TQueue Order -> StatusMap -> TChan CookFail -> IO ()
+cook cid fromQ toQ statusMap cookFail = cookLoop 1 -- Only allow for 1 mistake!
   where
     cookLoop :: Int -> IO ()
-    cookLoop 0 = 
-      putStrLn $ " Cook made too many mistakes, leaving early with no pay"
+    cookLoop 0 = do
+      putStrLn $ " Cook " ++ show cid ++ " made too many mistakes"
+      atomically $ writeTChan cookFail (CookFail cid)
     cookLoop n = do
       order <- atomically $ readTQueue fromQ
-      putStrLn $ " Cook " ++ show key ++ ", preparing burger for order " ++ show (orderId order)
+      putStrLn $ " Cook " ++ show cid ++ ", preparing burger for order " ++ show (orderId order)
       threadDelay =<< randomRIO (1_000_000, 2_000_000)
-      mistake <- randomRIO(1, 99 :: Int)
-      if mistake <= 10 
+      mistake <- randomRIO (1, 99 :: Int)
+      if mistake <= 10
         then do
-          putStrLn $ "  Cook " ++ show key ++ ", made a mistake on order " ++ show (orderId order)
+          putStrLn $ "  Cook " ++ show cid ++ ", made a mistake on order " ++ show (orderId order)
           atomically $ writeTQueue fromQ order -- put order back on original queue
           cookLoop $ n - 1
         else
           cookUpdate n order toQ statusMap
-
-    cookUpdate :: Int -> Order -> TQueue Order -> OSMap -> IO () 
+    cookUpdate :: Int -> Order -> TQueue Order -> StatusMap -> IO ()
     cookUpdate n order toQ statusMap = do
       atomically $ do
         updateStatus
@@ -103,19 +120,20 @@ cook key fromQ toQ statusMap = cookLoop 2
       -- Since food is cooked, put on pending delivery queue, for waitress to poll (to ensure drink is also done)
       atomically $ writeTQueue toQ order
 
-      putStrLn $ "  Burger cooked for order " ++ show (orderId order) ++ " and ready to eat, assistant preparing drinks "
+      putStrLn $ "  Burger cooked for order " ++ show (orderId order) ++ " and ready to eat (thanks Cook " ++ show cid ++ ") assistant preparing drinks "
       _ <- forkIO $ prepareDrink order statusMap
 
-      cookLoop n 
+      cookLoop n
 
-waitress :: Int -> TQueue Order -> TQueue Order -> OSMap -> IO ()
+waitress :: Int -> TQueue Order -> TQueue Order -> StatusMap -> IO ()
 waitress wid fromQ toQ statusMap = forever $ do
-  -- A) this simulates waitress checking if the entire order is done (e.g. pinging cooks)
+  -- A) this simulates waitress checking if the entire order is done (e.g. pestering cooks)
   order1 <- atomically $ readTQueue fromQ
   atomically $ do
     smap <- readTVar statusMap
     case HM.lookup (orderId order1) smap of
       Just status -> do
+        -- if expression false retry!
         check (orderState status == (Cooked :: OrderState))
         writeTQueue toQ order1
       Nothing -> return ()
@@ -132,7 +150,7 @@ waitress wid fromQ toQ statusMap = forever $ do
       )
   putStrLn $ " Waitress " ++ show wid ++ ", delivered order " ++ show (orderId order2) ++ " to customer "
 
-prepareDrink :: Order -> OSMap -> IO ()
+prepareDrink :: Order -> StatusMap -> IO ()
 prepareDrink order statusMap = do
   -- simulate drink preparation
   threadDelay =<< randomRIO (100_000, 200_000)
@@ -146,7 +164,7 @@ prepareDrink order statusMap = do
       )
   putStrLn $ " Drink prepared for order " ++ show (orderId order)
 
-updateStatus :: Int -> OSMap -> (OrderStatus -> OrderStatus) -> STM ()
+updateStatus :: Int -> StatusMap -> (OrderStatus -> OrderStatus) -> STM ()
 updateStatus key statusMap f = do
   modifyTVar' statusMap (HM.adjust f key)
 
@@ -164,7 +182,9 @@ main = do
   putStrLn "Restaurant simulation starting with 2 order takers, 3 cooks, 5 patrons, and  3 waitresses"
 
   replicateM_ 2 $ forkIO $ orderTaker customerQ kitchenQ statusMap
-  forM_ [1 .. 3] $ \cid -> forkIO $ cook cid kitchenQ pendingDeliveryQ statusMap
+
+  spawnCooks [1 .. 3] kitchenQ pendingDeliveryQ statusMap
+
   forM_ [1 .. 5] $ \pid -> forkIO $ patron pid customerQ
   forM_ [1 .. 3] $ \wid -> forkIO $ waitress wid pendingDeliveryQ deliveryQ statusMap
   threadDelay 50_000_000
